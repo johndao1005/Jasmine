3241d9fc23b9eacbee2b15da312c2cc7cf469393
johndao1005 <john.d1729@gmail.com>
2021-08-02 14:32:54 +1200

    finished the test case from last week

--- project/calculator.spec.js
+++ project/calculator.spec.js
@@ -10,16 +10,72 @@ describe('calculator.js', function() {
         calculator.substract(5)
         expect(calculator.total).toBe(25)
     })
-    xit('should add numbers to total', function() {
+    it('should multiply numbers to total', function() {
         const calculator = new Calculator()
         calculator.total = 20
         calculator.multiply(5)
         expect(calculator.total).toBe(100)
     })
-    xit('should add numbers to total', function() {
+    it('should divide numbers to total', function() {
         const calculator = new Calculator()
         calculator.total = 100
         calculator.divide(5)
         expect(calculator.total).toBe(20)
     })
+    xit('should throw an error if total not a number', function() {
+        const calculator = new Calculator()
+        expect(calculator.total).tobe(0)
+        expect(calculator.total).toBeFalsy()
+    })
+    it('has constructor', function() {
+        const calculator = new Calculator()
+        const calculator2 = new Calculator()
+        expect(calculator).toEqual(calculator2)
+    })
+    it('has instantiated', function() {
+        const calculator = new Calculator()
+        const calculator2 = new Calculator()
+        expect(calculator).toBeTruthy()
+        expect(calculator2).toBeTruthy()
+        expect(calculator).toEqual(calculator2)
+        expect(calculator.constructor.name).toContain('Cal')
+    })
+    it('has instantiated unique object', function() {
+        const calculator = new Calculator()
+        const calculator2 = new Calculator()
+        expect(calculator).not.toBe(calculator2)
+    })
+    it('has common operations', function() {
+        const calculator = new Calculator()
+        expect(calculator.add).not.toBeUndefined()
+        expect(calculator.substract).not.toBeUndefined()
+        expect(calculator.multiply).not.toBeUndefined()
+        expect(calculator.divide).not.toBeUndefined()
+    })
+    it('overwrite value', function() {
+        const calculator = new Calculator()
+        calculator.total = null
+        expect(calculator.total).toBeNull()
+    })
+    it('does not handle NaN', function() {
+        const calculator = new Calculator()
+        calculator.total = 20
+        calculator.multiply('john')
+        expect(calculator.total).toBeNaN()
+    })
+    xit('handle divide by 0', function() {
+        const calculator = new Calculator()
+        calculator.total = 20
+        expect(function() { calculator.divide(0) }).toThrow() //Just general error
+        expect(function() { calculator.divide(0) }).toThrowError(Error) //throw specific Error
+        expect(function() { calculator.divide(0) }).toThrowError(Error, 'Cannot divide by 0') //display the error message
+    })
+    it('return total', function() {
+        const calculator = new Calculator()
+        calculator.total = -50
+        expect(calculator.add(20)).toBe(-30)
+        expect(calculator.total).toMatch(/-?\d+/)
+        expect(typeof calculator.total).toMatch('number')
+    })
+
 })
